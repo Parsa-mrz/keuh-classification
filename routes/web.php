2fb83def93d618b5b247bcb155be25b3a419840e
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2023-05-29 15:11:35 +0330

    Add Routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[IndexController::class,'index'] )->name('index');
+Route::get('/cart',[CartController::class,'index'] )->name('cart');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
